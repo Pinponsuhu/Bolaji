697a43b6b06428ae4750cec4b322dd1cfb5c5cb1
Pinponsuhu Joseph <Pinponsuhuomorinsola@gmail.com>
2023-09-26 20:06:26 +0100

    Hi

---
+++ app/src/main/java/com/example/bolaji_o/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bolaji_o
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.check_out)
+    }
+}
